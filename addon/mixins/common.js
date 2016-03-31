8feb61156cabb55c85613e78035b7f873fc91305
Ken Snyder <ken@ken.net>
2016-03-31 12:10:50 +0100

    improved viewBox and aspect ratio

--- addon/mixins/common.js
+++ addon/mixins/common.js
@@ -15,17 +15,41 @@ export default Ember.Mixin.create({
       this.notifyPropertyChange('src');
     });
   },
+  preserveAspectRatio: null,
   setSVG(data) {
     const {width, height, x, y} = this.getProperties('width', 'height', 'x', 'y');
     this.set('svg', htmlSafe(Ember.$(data).find('svg').html()));
+
+    /**
+     * viewBox
+     *
+     * Not required but often included;
+     */
     const viewBox = Ember.$(data).find('svg').attr('viewBox');
-    if (viewBox) { this.set('viewBox', viewBox); }
+    if (Ember.typeOf(this.get('viewBox')) === 'undefined' && viewBox) { this.set('viewBox', viewBox); }
 
-    const defaultWidth = Ember.$(data).find('svg').attr('width');
-    const defaultHeight = Ember.$(data).find('svg').attr('height');
+    /**
+     * viewport (width, height)
+     *
+     * The visible portion of the SVG. Firefox may object if not defined but isn't
+     * strictly required and you run the risk of cutoff.
+     *
+     * By default if not set on component or in the SVG then a width of 100% will be
+     * used; this is typically the desired behaviour.
+     */
+    const viewWidth = Ember.$(data).find('svg').attr('width');
+    const viewHeight = Ember.$(data).find('svg').attr('height');
+    if (!width) { this.set('width', viewWidth || '100%'); }
+    if (!height && !width && viewHeight) { this.set('height', viewHeight); }
 
-    if (!width) { this.set('width', defaultWidth || '100%'); }
-    if (!height && !width && defaultHeight) { this.set('height', defaultHeight); }
+    /**
+     * Aspect Ratio (preserveAspectRatio)
+     *
+     * SVG has a boolean flag to ensure that viewBox and viewport are the same aspect
+     * ratio. If its stated in the file we'll proxy it through but default its not set.
+     */
+    const preserveAspectRatio = Ember.$(data).find('svg').attr('preserveAspectRatio');
+    if (preserveAspectRatio) { this.set('preserveAspectRatio', preserveAspectRatio); }
 
     const defaultX = Ember.$(data).find('svg').attr('x');
     const defaultY = Ember.$(data).find('svg').attr('y');
