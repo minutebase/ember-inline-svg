f68b763745f7fb502332eabdf6486ba09634d444
Raido Kuli <raido357@gmail.com>
2021-11-23 22:06:00 +0200

    Upgrade to Ember v3.28.x

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ Displays SVG images inline.
 
 ## Compatibility
 
-* Ember.js v3.4 or above
-* Ember CLI v2.13 or above
-* Node.js v8 or above
+* Ember.js v3.20 or above
+* Ember CLI v3.20 or above
+* Node.js v12 or above
 
 ## Installation
 
--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,10 +1,10 @@
 import EmberRouter from '@ember/routing/router';
-import config from './config/environment';
+import config from 'dummy/config/environment';
 
-const Router = EmberRouter.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
-});
+export default class Router extends EmberRouter {
+  location = config.locationType;
+  rootURL = config.rootURL;
+}
 
 Router.map(function() {
   this.route("root");
@@ -13,5 +13,3 @@ Router.map(function() {
   this.route("extension");
   this.route("title");
 });
-
-export default Router;
